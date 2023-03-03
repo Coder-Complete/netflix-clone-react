7bc2dcbe43dbea2b7100da1e45e99894557b385a
Jared <weissjmw@gmail.com>
2023-03-03 15:49:13 -0500

    up to useeffect explanation

--- src/TestOutUseEffect.js
+++ src/TestOutUseEffect.js
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from "react";
 
 function TestOutUseEffect() {
   const [count, setCount] = useState(0);
+  const [countTwo, setCountTwo] = useState(0);
 
   useEffect(() => {
     console.log("ran useeffect");
-  }, []);
+  }, [countTwo]);
+
+  useEffect(() => {
+    console.log("count useeffect ran");
+  }, [count]);
 
   return (
     <div>
       <p>count: {count}</p>
       <button onClick={() => setCount(count + 1)}>increase count</button>
+      <p>count two: {countTwo}</p>
+      <button onClick={() => setCountTwo(countTwo + 1)}>increase count</button>
     </div>
   );
 }
