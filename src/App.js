4d61b86ed047a9cb0d02ae5845b8c9865a4319e0
Jared <weissjmw@gmail.com>
2023-03-08 20:31:47 -0500

    clean up demo stuff, icons working, profile avatar, tooltips for nav bar

--- src/App.js
+++ src/App.js
@@ -2,56 +2,46 @@ import "./style.css";
 
 import React, { useEffect, useState } from "react";
 
-import Counter from "./Counter.js";
 import Header from "./Header.js";
 import Modal from "./Modal.js";
 import MovieRow from "./MovieRow.js";
-import TestOutUseEffect from "./TestOutUseEffect";
-import TryMapInComponent from "./TryMapInComponent";
 
 const App = () => {
   const [openModal, setOpenModal] = useState(false);
-  // const [rowData, setRowData] = useState([]);
+  const [rowData, setRowData] = useState([]);
 
-  // useEffect(() => {
-  //   let token =
-  //     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGQ0MmUyMzQwYWViOWQ0YzY5MTE4NWNiZGJjMDBmMiIsInN1YiI6IjYzZWVjZTY0Y2RkYmJjMDA3YzJiZDFhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q73VtBZhZdCb7hHnBBA5z3FOb9FqF9luAv9Kf2-OX9I";
+  useEffect(() => {
+    let token =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGQ0MmUyMzQwYWViOWQ0YzY5MTE4NWNiZGJjMDBmMiIsInN1YiI6IjYzZWVjZTY0Y2RkYmJjMDA3YzJiZDFhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q73VtBZhZdCb7hHnBBA5z3FOb9FqF9luAv9Kf2-OX9I";
 
-  //   let options = {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   };
+    let options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
 
-  //   let promise1 = fetch("https://api.themoviedb.org/3/list/1", options);
-  //   let promise2 = fetch("https://api.themoviedb.org/3/list/2", options);
-  //   let promise3 = fetch("https://api.themoviedb.org/3/list/3", options);
+    let promise1 = fetch("https://api.themoviedb.org/3/list/1", options);
+    let promise2 = fetch("https://api.themoviedb.org/3/list/2", options);
+    let promise3 = fetch("https://api.themoviedb.org/3/list/3", options);
 
-  //   Promise.all([promise1, promise2, promise3]).then((responses) => {
-  //     let promises = responses.map((response) => response.json());
-  //     Promise.all(promises).then((allData) => {
-  //       setRowData(allData);
-  //     });
-  //   });
-  // }, []);
+    Promise.all([promise1, promise2, promise3]).then((responses) => {
+      let promises = responses.map((response) => response.json());
+      Promise.all(promises).then((allData) => {
+        setRowData(allData);
+      });
+    });
+  }, []);
 
   return (
     <div className="app">
       <Header setOpenModal={setOpenModal} />
       <main>
         {openModal ? <Modal setOpenModal={setOpenModal} /> : null}
-        <TestOutUseEffect />
-        <br />
-        <br />
-        <br />
-        <br />
-        {/* <Counter /> */}
-        {/* <TryMapInComponent listOfNums={[55, 32, 443, 54, 25, 6]} /> */}
-        {/* {rowData.map((rowDataObject, index) => (
+        {rowData.map((rowDataObject, index) => (
           <MovieRow key={rowDataObject.id} index={index} data={rowDataObject} />
-        ))} */}
+        ))}
       </main>
     </div>
   );
