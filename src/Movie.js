421aabc8295210437dc5c6fc497090208cdbe4a2
Jared <weissjmw@gmail.com>
2023-03-13 16:48:49 -0400

    carousel generally working, overflow wont work on movie container b/c movie details wont show, would need to separate movie modal into sep component

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,10 @@
 import "./Movie.css";
 
+import { GiEnergyShield } from "react-icons/gi";
 import React from "react";
 
 function Movie({ data }) {
   const imgUrl = "https://image.tmdb.org/t/p/w500";
-  console.log(data);
   return (
     <div className="movie">
       <div className="movie__image-container">
@@ -13,14 +13,14 @@ function Movie({ data }) {
       <div className="movie-details">
         <div className="movie-details__top-row"></div>
         <div className="movie-details__info-line">
-          <span class="green">50% Match</span>
+          <span className="green">50% Match</span>
           <span className="border">TV-MA</span>
           <span>6 Seasons</span>
           <span className="border">HD</span>
         </div>
         <div className="movie-details__genres">
           {data.genre_ids.map((genreId) => (
-            <span>{genreId}</span>
+            <span key={`movie-${data.id}-genre-${genreId}`}>{genreId}</span>
           ))}
         </div>
       </div>
--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,23 +1,59 @@
 import "./MovieRow.css";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Movie from "./Movie";
 
 const MovieRow = (props) => {
-  const [page, setPage] = useState(1);
+  const [slide, setSlide] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  // set spec width for movie
-  // see width of screen
-  // calc how many imgs can be shown
-  // calc how many "pages" of imgs there can be
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth);
+    });
+  }, []);
+
+  function numOfImagesPerSlide() {
+    // 154 is width of movie including margin
+    // +1 so that images at ends can be cut off
+    return Math.floor(windowWidth / 154) + 1;
+  }
+
+  function maxNumOfSlides(numOfMovies) {
+    return Math.ceil(numOfMovies / numOfImagesPerSlide());
+  }
+
+  function getDataSliceForSlide() {
+    let amountPerSlide = numOfImagesPerSlide();
+    let start = slide * amountPerSlide;
+    let end = slide * amountPerSlide + amountPerSlide;
+    return props.data.items.slice(start, end);
+  }
+
   return (
     <div className="row">
       <h4>{props.data.name}</h4>
       <div className="images-container">
-        {props.data.items.map((movie) => (
-          <Movie data={movie} key={`${Math.random()}-${movie.id}`} />
+        <div
+          className="left-arrow"
+          onClick={() => {
+            if (slide > 0) setSlide(slide - 1);
+          }}
+        >
+          {"<"}
+        </div>
+        {getDataSliceForSlide().map((movie, i) => (
+          <Movie data={movie} key={`movie-${movie.id}`} />
         ))}
+        <div
+          className="right-arrow"
+          onClick={() => {
+            if (slide !== maxNumOfSlides() - 1) setSlide(slide + 1);
+          }}
+        >
+          {">"}
+        </div>
       </div>
     </div>
   );
