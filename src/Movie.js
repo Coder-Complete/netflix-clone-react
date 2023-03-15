fd2182d8cb060e8970179e5f170182bbf7761bff
Jared <weissjmw@gmail.com>
2023-03-14 22:31:15 -0400

    carousel working

--- src/Movie.js
+++ src/Movie.js
@@ -4,21 +4,21 @@ import React from "react";
 
 function Movie({ data }) {
   const imgUrl = "https://image.tmdb.org/t/p/w500";
-  console.log(data);
+  // console.log(data);
   return (
     <div className="movie">
       <img src={imgUrl + data.poster_path} alt={data.name} />
       <div className="movie-details">
         <div className="movie-details__top-row"></div>
         <div className="movie-details__info-line">
-          <span class="green">50% Match</span>
+          <span className="green">50% Match</span>
           <span className="border">TV-MA</span>
           <span>6 Seasons</span>
           <span className="border">HD</span>
         </div>
         <div className="movie-details__genres">
           {data.genre_ids.map((genreId) => (
-            <span>{genreId}</span>
+            <span key={genreId}>{genreId}</span>
           ))}
         </div>
       </div>
--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,18 +1,67 @@
 import "./MovieRow.css";
 
-import Movie from "./Movie";
-import React from "react";
+import React, { useState, useSyncExternalStore } from "react";
 
-const MovieRow = (props) => {
+import Movie from "./Movie";
+
+const MovieRow = ({ data }) => {
+  const [page, setPage] = useState(0);
+
+  let movieWidth = 150;
+  let movieMarginLeft = 2;
+  let movieMarginRight = 2;
+  let totalMovieWidth = movieWidth + movieMarginLeft + movieMarginRight;
+
+  // get number of movies we can fit on screen
+  function getNumberOfMoviesPerPage() {
+    return Math.floor(window.innerWidth / totalMovieWidth);
+  }
+
+  // get number of pages
+  function getLastPageNumber() {
+    let numOfMoviesTotal = data.items.length;
+    let numOfMoviesPerPage = getNumberOfMoviesPerPage();
+    let offsetDueToPageStartingAtZeroIndex = 1;
+    return (
+      Math.ceil(numOfMoviesTotal / numOfMoviesPerPage) -
+      offsetDueToPageStartingAtZeroIndex
+    );
+  }
+
+  function getArrayOfMovies() {
+    let allMovies = data.items;
+    let numOfMoviesPerPage = getNumberOfMoviesPerPage();
+    let beginning = page * numOfMoviesPerPage;
+    let end = page * numOfMoviesPerPage + numOfMoviesPerPage;
+    return allMovies.slice(beginning, end);
+  }
+
+  let arrayOfMoviesForCurrentPage = getArrayOfMovies();
+  let lastPageNumber = getLastPageNumber();
+
   return (
     <div className="row">
-      <h4>{props.data.name}</h4>
+      <h4>{data.name}</h4>
       <div className="images-container">
-        <div className="left-arrow">{"<"}</div>
-        {props.data.items.map((movie) => (
+        <div
+          className="left-arrow"
+          onClick={() => {
+            if (page !== 0) setPage(page - 1);
+          }}
+        >
+          {"<"}
+        </div>
+        {arrayOfMoviesForCurrentPage.map((movie) => (
           <Movie data={movie} key={`${movie.id}`} />
         ))}
-        <div className="right-arrow">{">"}</div>
+        <div
+          className="right-arrow"
+          onClick={() => {
+            if (page !== lastPageNumber) setPage(page + 1);
+          }}
+        >
+          {">"}
+        </div>
       </div>
     </div>
   );
