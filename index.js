2009db0ab4af61069bf599add5fd6ce094576ac8
Jared <weissjmw@gmail.com>
2023-05-16 17:07:47 +0300

    added rr, app is in / route

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import React, { StrictMode } from "react";
-
-import App from "./src/App.js";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./src/App.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const container = document.querySelector("#root");
 
 const root = createRoot(container);
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
